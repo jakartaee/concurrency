676c949cd0ae5600d38e7d4662cec0d9e371acad
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-07-07 15:39:36 -0500

    Update TCK version
    Signed-off-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck-dist/src/main/artifacts/artifact-install.sh
+++ tck-dist/src/main/artifacts/artifact-install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 ## A sample script to install the artifact directory contents into a local maven repository
-VERSION=3.0.0
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+  VERSION="$1"
+else
+  VERSION="3.0.1"
+fi
 
 # Parent pom
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
