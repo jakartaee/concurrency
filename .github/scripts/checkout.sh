c956066766794e50d63a2b50fbdc8be533bbbb06
Kyle Aure <KyleJAure@gmail.com>
2023-11-27 16:32:50 -0600

    Fix builds that open a pull request

---
+++ .github/scripts/checkout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "The checkout.sh script requires exactly 1 argument"
+    exit 1
+fi
+
+branch=$1
+
+if [ "$(git ls-remote --heads origin refs/heads/$branch | wc -l)" -eq "1" ]; then
+    git fetch origin
+    git checkout -t origin/$branch
+    echo "branch_existed=true" >> $GITHUB_OUTPUT
+else
+    git checkout -b $branch
+    echo "branch_existed=false" >> $GITHUB_OUTPUT
+fi
