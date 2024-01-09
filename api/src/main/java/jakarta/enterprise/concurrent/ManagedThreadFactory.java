7738c6dd39a4df1f01fa75047d8c896b957ac5c4
Nathan Rauh <nathan.rauh@us.ibm.com>
2024-01-09 14:30:22 -0600

    Issue #268 document additional limitations if using virtual threads
    Signed-off-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- api/src/main/java/jakarta/enterprise/concurrent/ManagedThreadFactory.java
+++ api/src/main/java/jakarta/enterprise/concurrent/ManagedThreadFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -34,8 +34,9 @@ import java.util.concurrent.ThreadFactory;
  * Component Provider and Deployer identify these requirements and map the
  * resource environment reference appropriately.<p>
  *
- * Threads returned from the {@code newThread()} method should implement the
- * {@link ManageableThread} interface.
+ * Platform threads returned from the {@code newThread()} method should implement the
+ * {@link ManageableThread} interface. Virtual threads do not implement
+ * {@code ManageableThread}.
  *
  * The Runnable task that is allocated to the new thread using the
  * {@link ThreadFactory#newThread(Runnable)} method
