3de936171dfacbd1c35554873d0bbbc43194bf7d
Piotrek Zygielo <piotr.zygielo@gmail.com>
2026-01-14 20:59:22 +0100

    Drop unnecessary semicolon

--- api/src/main/java/jakarta/enterprise/concurrent/ManagedExecutors.java
+++ api/src/main/java/jakarta/enterprise/concurrent/ManagedExecutors.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2010, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -31,7 +32,7 @@ public final class ManagedExecutors {
      * Not suppose to create instances of this class.
      */
     private ManagedExecutors() {
-    };
+    }
 
     /**
      * Error message for null tasks.
