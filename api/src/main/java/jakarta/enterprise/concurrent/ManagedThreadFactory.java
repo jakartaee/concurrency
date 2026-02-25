038e0fca184073280148fe7d2428b6f1fb4c90d1
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-02-25 10:15:26 -0600

    Align ManagedThreadFactory Javadoc requirement with the spec requirement

--- api/src/main/java/jakarta/enterprise/concurrent/ManagedThreadFactory.java
+++ api/src/main/java/jakarta/enterprise/concurrent/ManagedThreadFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -34,9 +34,9 @@ import java.util.concurrent.ThreadFactory;
  * Component Provider and Deployer identify these requirements and map the
  * resource environment reference appropriately.<p>
  *
- * Platform threads returned from the {@code newThread()} method should implement the
+ * Platform threads returned from the {@code newThread()} method must implement the
  * {@link ManageableThread} interface. Virtual threads do not implement
- * {@code ManageableThread}.
+ * {@code ManageableThread}.<p>
  *
  * The Runnable task that is allocated to the new thread using the
  * {@link ThreadFactory#newThread(Runnable)} method
